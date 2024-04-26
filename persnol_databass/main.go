f78c119d1305b22d0a3954f20ade7c14c93033a1
Pawan <pawansharma93520@gmail.com>
2024-04-26 20:17:46 +0530

    persnol databass added some function left

---
+++ persnol_databass/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+var choice int
+
+type User struct {
+	Name    string  `json:"name"`
+	Email   string  `json:"email"`
+	PhoneNo string  `json:"phone_no"`
+	Address Address `json:"address"`
+}
+type Address struct {
+	Street  string `json:"street"`
+	City    string `json:"city"`
+	State   string `json:"state"`
+	PinCode string `json:"pin_code"`
+}
+
+var userDetails = map[string][]User{
+	"8930008915": {User{Name: "Vikash", Email: "vikash@123", PhoneNo: "893008915", Address: Address{Street: "Street 1", City: "City1", State: "Haryana", PinCode: "123456"}}},
+	"7988323110": {User{Name: "Pawan", Email: "pawan@123", PhoneNo: "7988323110", Address: Address{Street: "Street 3", City: "City1", State: "Haryana", PinCode: "123456"}}},
+	"8295068010": {User{Name: "Ashu", Email: "ashu@123", PhoneNo: "8295068010", Address: Address{Street: "Street 2", City: "City1", State: "Haryana", PinCode: "123"}}},
+}
+
+func greet() {
+	fmt.Println("Welcome to our application enter your choice")
+	fmt.Println("1: Print existing data")
+	fmt.Println("2: Search by phone number")
+	fmt.Println("3: Add new member")
+
+}
+func PrintVal() {
+	data, err := json.MarshalIndent(userDetails, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var printData = fmt.Sprint(string(data))
+	fmt.Println(printData)
+}
+func searchByPhone(PhoneNo string) {
+
+	fmt.Println(userDetails[PhoneNo])
+
+}
+func addMember() {
+
+}
+func main() {
+
+	greet()
+	fmt.Println("Enter Here!")
+	fmt.Scan(&choice)
+	switch choice {
+	case 1:
+		PrintVal()
+	case 2:
+		fmt.Println("Enter phone number to search")
+		var searchPhoneNo string
+		fmt.Scan(&searchPhoneNo)
+		searchByPhone(searchPhoneNo)
+	case 3:
+		var firstName string
+		var email string
+		var phoneNo string
+		// var address Address
+		fmt.Println("Enter first name:")
+		fmt.Scan(&firstName)
+		fmt.Println("Enter email id:")
+		fmt.Scan(&email)
+		fmt.Println("Enter Your Phone number:")
+		fmt.Scan(&phoneNo)
+	default:
+		fmt.Println("Invalid choice")
+	}
+}
